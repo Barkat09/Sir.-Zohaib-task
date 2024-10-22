c14397f3816315a2f4ebf88b918a6f9ee7f665e5
Barkat09 <soomrobarkat3345@gmail.com>
2024-10-22 11:56:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// Get the button element
+const alertButton = document.getElementById('alertButton');
 
+// Add a click event listener to the button
+alertButton.addEventListener('click', function() {
+    alert('Button clicked! Welcome to my website!');
+});
